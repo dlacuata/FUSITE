98975d9759c17ce1f66c30cdcfbaa2372fadbaf2
Harrison Tran <harrit4@uci.edu>
2025-08-09 23:53:59 -0700

    responsive design for intern page

--- src/app/page.js
+++ src/app/page.js
@@ -13,12 +13,17 @@ const inter = Inter({
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center h-[442px] pt-[160px] pr-[354px] pl-[354px]">
+      <div className="flex flex-col items-center 
+                      md:justify-center  
+                      lg:h-[442px] md:h-[457px] h-[329px]
+                      lg:pr-[354px] md:pr-[32px] pr-[16px]
+                      lg:pl-[354px] md:pl-[32px] pl-[16px]
+                      bg-gray-100">
         <div className={livvic.className}>
-          <div className="h-[45px] text-3xl">Intern with FUSION!</div>
+          <div className="h-[45px] md:text-3xl text-xl">Intern with FUSION!</div>
         </div>
         <div className={inter.className}>
-          <div className="h-[160px] text-xl text-intern mt-4 text-center">
+          <div className="h-[160px] md:text-xl text-base text-intern md:mt-4 text-center">
             Our 10-week internship program enables interns to shadow three board positions of their 
             choice. The first six weeks involve rotating through these roles, followed by a focus on one 
             preferred position during the final four weeks, culminating in an intern-led general meeting in 

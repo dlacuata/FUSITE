116d383a1fb2d09ff92fabf324cd4daa946e0893
Derrick Thrower <throwerderrick24@gmail.com>
2025-08-16 03:33:34 -0700

    basic layout structure complete.

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,27 @@ export default function Home() {
         <div className="bg-[#D9D9D9] flex-shrink-0 w-full max-w-[335pc] h-[209px] md:w-[332px] md:h-[565px] xl:w-[517px] xl:h-[360px]"></div>{/* placeholder for a picture */}
       </div>
 
-      <div>second info box</div>
+      <div className="w-full min-h-[820px] pt-[64px] px-[20px] pb-[64px] bg-white flex gap-[40px] flex-col-reverse items-center justify-center 
+      md:min-h-[643px] md:pt-[100px] md:pr-[32px] md:pl-[32px] md:pb-[100px] md:flex-row-reverse 
+      lg:min-h-[635px] lg:pt-[160px] lg:pr-[138px] lg:pl-[138px] lg:pb-[160px] lg:gap-[60px] lg:flex-row-reverse">
+
+        <div className="flex flex-col text-left gap-[19px] w-full max-w-[335px] md:w-[332px] lg:w-[587px]">
+          <h1 className="text-black font-livvic font-bold text-4xl leading-none tracking-normal 
+          w-full max-w-[335px]
+          h-[135px]">
+            A Family that Celebrates Diversity and Dedication
+          </h1>
+
+          <p className="text-gray-600 font-inter font-normal text-base leading-8 
+          w-full max-w-[335px] h-fit max-h-[]
+          md:w-[332px] md:h[352px]
+          lg:w-[587] lg:h[192px]">
+            At FUSION, we strive for inclusivity as we welcome students with an array of cultural backgrounds in STEM. Taking pride in our Support pillar, we find many ways to encourage our members to grow academically and professionally as a collective, allowing all to share and learn with one another. Join us as we continue to build a community where diversity thrives and STEM continues to grow with our intelligence.
+          </p>
+          <button className=" w-[130px] h-[40px] self-center md:self-start font-medium bg-[#1447E6] pt-2.5 pr-4 pb-2.5 pl-4 gap-1.5 rounded-xl text-center text-white">Learn More &gt; </button>
+        </div>
+        <div className="bg-[#D9D9D9] flex-shrink-0 w-full max-w-[335pc] h-[209px] md:w-[332px] md:h-[565px] xl:w-[517px] xl:h-[360px]"></div>{/* placeholder for a picture */}
+      </div>
 
 
 
